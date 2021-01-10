541251f038245899fffbbb6c9fb8beea301f384f
Brayan <brayandeyvid17@gmail.com>
2021-01-09 23:16:05 -0300

    conversor v3

--- 3-Third Project/conversor/lib/main.dart
+++ 3-Third Project/conversor/lib/main.dart
@@ -7,18 +7,26 @@ const request =
     "https://api.hgbrasil.com/finance?format=json-cors&key=47a5af8f";
 
 void main() async {
-  print(await getData());
   runApp(MaterialApp(
-    title: "Conversor de Moedas",
-    home: Home(),
-  ));
+      title: "Conversor de Moedas",
+      home: Home(),
+      theme: ThemeData(
+          hintColor: Colors.amber,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            focusedBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+            hintStyle: TextStyle(color: Colors.amber),
+          ))));
 }
 
 Future<Map> getData() async {
   //a api retorna um dado do futuro
   http.Response response = await http.get(request); //await espera o dado chegar
-  return json
-      .decode(response.body); //pega um arquivo json e transforma em um mapa
+  return json.decode(response.body);
+  /*pega um arquivo json e transforma em um mapa*/
 }
 
 class Home extends StatefulWidget {
@@ -27,6 +35,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dollar;
+  double euro;
+
+  final realController = TextEditingController();
+  final dollarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  void _realChanged(String text) {
+    print(text);
+  }
+
+  void _dollarChanged(String text) {
+    print(text);
+  }
+  void _euroChanged(String text) {
+    print(text);
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
@@ -37,9 +63,10 @@ class _HomeState extends State<Home> {
       ),
       body: FutureBuilder<Map>(
           future: getData(),
-          builder: (context, snapshot) { //snapshot vê o estado da conexão
+          builder: (context, snapshot) {
+            //snapshot vê o estado da conexão
             switch (snapshot.connectionState) {
-              case ConnectionState.none:  //caso não esteja conectado
+              case ConnectionState.none: //caso não esteja conectado
               case ConnectionState.waiting: //₢aso esteja esperando os dados
                 return Center(
                   child: Text(
@@ -49,7 +76,8 @@ class _HomeState extends State<Home> {
                   ),
                 );
               default:
-                if (snapshot.hasError) { //caso aja algum erro na conexão 
+                if (snapshot.hasError) {
+                  //caso aja algum erro na conexão
                   return Center(
                     child: Text(
                       "Erro ao Carregar Dados :(",
@@ -57,14 +85,48 @@ class _HomeState extends State<Home> {
                       textAlign: TextAlign.center,
                     ),
                   );
-                }else{
-                  return Container(
-                    color: Colors.green,
+                } else {
+                  dollar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(
+                          Icons.monetization_on,
+                          size: 150.0,
+                          color: Colors.amber,
+                        ),
+                        buildTextField(
+                            "Reais", "R\$", realController, _realChanged),
+                        Divider(),
+                        buildTextField("Dollars", "US\$", dollarController,
+                            _dollarChanged),
+                        Divider(),
+                        buildTextField(
+                            "Euros", "€", euroController, _euroChanged),
+                      ],
+                    ),
                   );
-                }              
+                }
             }
-          }
-      ),
+          }),
     );
   }
+}
+
+Widget buildTextField(
+    String label, String prefix, TextEditingController c, Function f) {
+  return TextField(
+    controller: c,
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix),
+    style: TextStyle(fontSize: 25.0, color: Colors.amber),
+    onChanged: f,
+    keyboardType: TextInputType.number,
+  );
 }
