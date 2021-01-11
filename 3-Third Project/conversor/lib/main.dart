9c53205fb7d2c0c7df0be197820c06147d43f4bf
Brayan <brayandeyvid17@gmail.com>
2021-01-11 17:13:52 -0300

    Conversor VF

--- 3-Third Project/conversor/lib/main.dart
+++ 3-Third Project/conversor/lib/main.dart
@@ -42,19 +42,43 @@ class _HomeState extends State<Home> {
   final dollarController = TextEditingController();
   final euroController = TextEditingController();
 
+  void _clearAll() {
+    realController.text = "";
+    dollarController.text = "";
+    euroController.text = "";
+  }
+  void _resetFields (){
+    realController.text = "";
+    dollarController.text = "";
+    euroController.text = "";
+
+  }
+
   void _realChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
     double real = double.parse(text);
     dollarController.text = (real / dollar).toStringAsFixed(3);
     euroController.text = (real / euro).toStringAsFixed(3);
   }
 
   void _dollarChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
     double dollar = double.parse(text);
     realController.text = (dollar * this.dollar).toStringAsFixed(3);
     euroController.text = ((dollar * this.dollar) / euro).toStringAsFixed(3);
   }
 
   void _euroChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
     double euro = double.parse(text);
     realController.text = (euro * this.euro).toStringAsFixed(3);
     dollarController.text = ((euro * this.euro) / dollar).toStringAsFixed(3);
@@ -67,6 +91,9 @@ class _HomeState extends State<Home> {
         title: Text("\$Conversor de Moeda\$"),
         centerTitle: true,
         backgroundColor: Colors.amber,
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.refresh), onPressed: _resetFields)
+        ],
       ),
       body: FutureBuilder<Map>(
           future: getData(),
